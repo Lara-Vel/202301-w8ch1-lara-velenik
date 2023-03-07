7f30216e169ea326e94c42e49897e91dd6a3ca57
Lara-Vel <lara.vel@outlook.com>
2023-03-07 17:07:37 +0100

    import Header

--- src/layout/MainLayout/MainLayout.tsx
+++ src/layout/MainLayout/MainLayout.tsx
@@ -1,6 +1,5 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../../features/header/Header';
+import { Header } from '../../features/header/Header';
 
 const MainLayout = () => {
   return (
