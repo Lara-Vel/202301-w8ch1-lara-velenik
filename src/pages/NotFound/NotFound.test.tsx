45045507782561cc4aafc21997808dbf0c6b7a05
Lara-Vel <lara.vel@outlook.com>
2023-03-07 18:52:21 +0100

    change test notfound

--- src/pages/NotFound/NotFound.test.tsx
+++ src/pages/NotFound/NotFound.test.tsx
@@ -5,7 +5,7 @@ import NotFound from './NotFound';
 describe('Given a page error 404', () => {
   test('When rendered, Then it should show all information of the page', () => {
     render(<NotFound />, { wrapper: MemoryRouter });
-    const titleElement = screen.getByText('Error 404');
+    const titleElement = screen.getByText('404 ERROR');
     const paragraphElement = screen.getByText(
       'You underestimate the power of the dark side'
     );
